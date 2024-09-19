17d8f30d1bdbdc7fbd1c6b35d24fdc775ba2b6b4
amandafr9 <160618671+amandafr9@users.noreply.github.com>
2024-09-19 11:31:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Qual o filme com maior bilheteria do mundo?',
+    'Avatar (2009) Arrecadação total: US$ 2,847 bilhões'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi a final e quem ganhou o primeiro mundial de clubes da FIFA?',
+    'Corinthians x Vasco, o grande campeão foi o Corinthians'
+)
+
+criaCartao(
+    'Filmes',
+    'Os três Homens-Aranha aparecem juntos em qual dos filmes?',
+    'Homem-Aranha Sem volta pra casa'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem fez o gol do corinthians no mundial de 2012?',
+    'O Alvinegro venceu o Chelsea por 1 a 0, com gol de ~Paolo Guerrero~, e conquistou o mundo pela segunda vez.'
+)
